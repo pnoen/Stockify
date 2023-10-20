b79bd5116c70402d4f5379c5fb695e61d30c19f9
rton8097 <rton8097@uni.sydney.edu.au>
2023-10-20 13:33:07 +1100

    changed the product detail styling

--- frontend/src/features/Catalogue/components/ProductDetailModal/index.js
+++ frontend/src/features/Catalogue/components/ProductDetailModal/index.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Modal, Box, Typography, Button, TextField } from "@mui/material";
+import {
+  Modal,
+  Box,
+  Typography,
+  Button,
+  TextField,
+  Divider,
+  Paper,
+} from "@mui/material";
 import { createDraftOrder, createOrderItem, getImageUrl } from "./api";
 import SuccessSnackBar from "../../../../components/Snackbars/SuccessSnackbar";
 
@@ -66,7 +74,7 @@ export default function ProductDetailModal({
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
-          bgcolor: "background.paper",
+          bgcolor: "#cbf5d6",
           boxShadow: 24,
           p: 4,
           width: "80%",
@@ -117,48 +125,50 @@ export default function ProductDetailModal({
             backgroundColor: "rgba(255, 255, 255, 0.6)",
             padding: "0.5em",
             borderRadius: "5px",
-            marginTop: "0.5em",
+            margin: "0.5em 0",
           }}
         >
           {product.businessCode}
         </Typography>
+        {/* <Divider /> */}
         {/* Centered Name and Description */}
-        <Typography variant="h4" gutterBottom align="center">
-          {product.name}
-        </Typography>
-        <Typography align="center">{product.description}</Typography>
-        <Typography variant="h6">Price: ${product.price}</Typography>
-        <Typography variant="h6">
-          Quantity Available: {product.quantity}
-        </Typography>
-
-        <Box
-          display="flex"
-          alignItems="center"
-          sx={{ marginBottom: "1em", paddingTop: "1vh" }}
-        >
-          <Typography variant="h6" sx={{ paddingRight: "1vh" }}>
-            Order Quantity:
+        <Paper sx={{ margin: "0em", padding: "1em", borderRadius: "5px" }}>
+          <Typography variant="h4" gutterBottom align="center">
+            {product.name}
           </Typography>
-          <TextField
-            type="number"
-            value={quantity}
-            onChange={handleQuantityChange}
-            InputProps={{
-              inputProps: {
-                min: 0,
-                max: product.quantity,
-              },
-            }}
-            sx={{
-              marginRight: "10px",
-              width: "12%",
-              "& .MuiInputBase-input": {
-                height: "0em",
-              },
-            }}
-          />
-        </Box>
+          <Typography align="center">{product.description}</Typography>
+          <Divider sx={{ margin: "1em 0" }} />
+          <Typography variant="h6">Price: ${product.price}</Typography>
+          <Typography variant="h6">
+            Quantity Available: {product.quantity}
+          </Typography>
+
+          <Box display="flex" alignItems="center">
+            <Typography variant="h6" sx={{ paddingRight: "1vh" }}>
+              Order Quantity:
+            </Typography>
+            <TextField
+              type="number"
+              value={quantity}
+              onChange={handleQuantityChange}
+              InputProps={{
+                inputProps: {
+                  min: 0,
+                  max: product.quantity,
+                },
+              }}
+              sx={{
+                marginRight: "10px",
+                width: "12%",
+                "& .MuiInputBase-input": {
+                  height: "0em",
+                },
+              }}
+            />
+          </Box>
+        </Paper>
+
+        {/* <Divider /> */}
 
         <Box
           mt={3}
@@ -168,11 +178,14 @@ export default function ProductDetailModal({
             alignItems: "center",
           }}
         >
-          <Button variant="outlined" onClick={onClose}>
+          <Button
+            variant="outlined"
+            onClick={onClose}
+            sx={{ backgroundColor: "rgba(255, 255, 255, 0.6)" }}
+          >
             Back
           </Button>
 
-          <Box></Box>
           <Button
             variant="contained"
             style={{ backgroundColor: "#1DB954", color: "white" }}
