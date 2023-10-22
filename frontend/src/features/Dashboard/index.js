41eec36cc69e4b93754317a1b3c4d6c7668aa12f
rton8097 <rton8097@uni.sydney.edu.au>
2023-10-22 14:34:51 +1100

    styled the dashboard page

--- frontend/src/features/Dashboard/index.js
+++ frontend/src/features/Dashboard/index.js
@@ -1,11 +1,18 @@
-// Dashboard.js
-
 import React, { useState, useEffect } from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Paper, Typography } from "@mui/material";
 import StatCard from "./components/StatCard";
 import { getBusinessDetails, getBusinessStats } from "./api";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  boldText: {
+    fontWeight: "bold",
+    fontFamily: "Your Nice Font, sans-serif", // Replace "Your Nice Font" with the desired font
+  },
+}));
 
 const Dashboard = () => {
+  const classes = useStyles();
   const [businessInfo, setBusinessInfo] = useState({
     businessName: "",
     businessCode: "",
@@ -37,19 +44,45 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <Box sx={{ flexGrow: 1, m: 3, maxWidth: "60%" }}>
-      <Typography variant="h2">Dashboard</Typography>
-      <Typography variant="h6" mb={3}>
-        Welcome {businessInfo.businessName}!
-      </Typography>
-      <Grid container spacing={3}>
-        {Object.entries(stats).map(([title, value], index) => (
-          <Grid item xs={4} key={index}>
-            <StatCard title={title} value={value} />
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: "5vh",
+      }}
+    >
+      <div style={{ width: "90%" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: "16px",
+          }}
+        >
+          <Typography variant="h3" gutterBottom className={classes.boldText}>
+            Dashboard
+          </Typography>
+        </div>
+
+        <Typography variant="h6" gutterBottom>
+          Welcome {businessInfo.businessName}!
+        </Typography>
+
+        <Paper style={{ borderRadius: "10px", padding: "16px" }}>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={3}>
+              {Object.entries(stats).map(([title, value], index) => (
+                <Grid item xs={4} key={index}>
+                  <StatCard title={title} value={value} />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Paper>
+      </div>
+    </div>
   );
 };
 
