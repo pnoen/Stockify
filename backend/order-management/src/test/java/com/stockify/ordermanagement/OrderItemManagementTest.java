73e4265e7d34cc477991695036f4fd893d9c99e4
Lily Jing <ljin7638@uni.sydney.edu.au>
2023-10-27 18:42:39 +1100

    Added CreateOrderItemTests

--- backend/order-management/src/test/java/com/stockify/ordermanagement/OrderItemManagementTest.java
+++ backend/order-management/src/test/java/com/stockify/ordermanagement/OrderItemManagementTest.java
@@ -1,5 +1,6 @@
 package com.stockify.ordermanagement;
 
+import com.stockify.ordermanagement.constants.OrderStatus;
 import com.stockify.ordermanagement.controller.OrderItemController;
 import com.stockify.ordermanagement.dto.*;
 import com.stockify.ordermanagement.model.Order;
@@ -18,15 +19,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 public class OrderItemManagementTest {
@@ -46,7 +41,80 @@ public class OrderItemManagementTest {
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+
+        Order mockOrder = new Order();
+        mockOrder.setOrganisation(1);
+        mockOrder.setCustomerId(1);
+        mockOrder.setBusinessCode(1); // example valid order for the tests
+        mockOrder.setInvoiceId(1);
+        mockOrder.setOrderDate(LocalDate.of(2023, 10, 9));
+        mockOrder.setCompletionDate(LocalDate.of(2023, 10, 9));
+        mockOrder.setTotalCost(1.0);
+        mockOrder.setOrderStatus(OrderStatus.COMPLETE);
+        mockOrder.setBusinessName("Stockify");
+        when(orderRepository.findById(1)).thenReturn(Optional.of(mockOrder));
+
         when(orderItemRepository.save(any(OrderItem.class))).thenReturn(new OrderItem());
+    }
+
+    @Test
+    public void testValidCreateOrderItem() {
+        // Create Valid Order Item
+        OrderItemRequest orderItemRequest = new OrderItemRequest();
+        orderItemRequest.setOrderId(1);
+        orderItemRequest.setProductId(1);
+        orderItemRequest.setBusinessCode(1);
+        orderItemRequest.setQuantity(1);
+        orderItemRequest.setLastUpdated(LocalDate.of(2023, 10, 9));
+        orderItemRequest.setPrice(1);
+
+        ResponseEntity<ApiResponse> response = orderItemController.createOrderItem(orderItemRequest);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void testInvalidCreateOrderItem() {
+        // Create Order Item Without Valid Existing Order
+        OrderItemRequest orderItemRequest = new OrderItemRequest();
+        orderItemRequest.setOrderId(0);
+        orderItemRequest.setProductId(1);
+        orderItemRequest.setBusinessCode(1);
+        orderItemRequest.setQuantity(1);
+        orderItemRequest.setLastUpdated(LocalDate.of(2023, 10, 9));
+        orderItemRequest.setPrice(1);
+
+        ResponseEntity<ApiResponse> response = orderItemController.createOrderItem(orderItemRequest);
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+    }
+
+    @Test
+    public void testInvalidCreateOrderItemPriceZero() {
+        // Create Order Item Without Valid Order Item Quantity
+        OrderItemRequest orderItemRequest = new OrderItemRequest();
+        orderItemRequest.setOrderId(1);
+        orderItemRequest.setProductId(1);
+        orderItemRequest.setBusinessCode(1);
+        orderItemRequest.setQuantity(0);
+        orderItemRequest.setLastUpdated(LocalDate.of(2023, 10, 9));
+        orderItemRequest.setPrice(1);
+
+        ResponseEntity<ApiResponse> response = orderItemController.createOrderItem(orderItemRequest);
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+    }
+
+    @Test
+    public void testInvalidCreateOrderItemWrongCode() {
+        // Create Order Item with Invalid Business Code
+        OrderItemRequest orderItemRequest = new OrderItemRequest();
+        orderItemRequest.setOrderId(1);
+        orderItemRequest.setProductId(1);
+        orderItemRequest.setBusinessCode(2);
+        orderItemRequest.setQuantity(1);
+        orderItemRequest.setLastUpdated(LocalDate.of(2023, 10, 9));
+        orderItemRequest.setPrice(1);
+
+        ResponseEntity<ApiResponse> response = orderItemController.createOrderItem(orderItemRequest);
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
     }
 
     @Test
